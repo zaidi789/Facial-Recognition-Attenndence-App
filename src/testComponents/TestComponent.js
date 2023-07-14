d263e6c2fdcb969a76b55028bcec7f8782b2cce8
Zaid Rafiq <zaidrafiq11@gmail.com>
2023-07-14 12:53:34 +0500

    Clean code till attandance file with react native face api working great

--- src/testComponents/TestComponent.js
+++ src/testComponents/TestComponent.js
@@ -1,4 +1,6 @@
-import React, {useEffect, useState} from 'react';
+//in this component i have changed the camera for two pictures one for face one simple then sscanerio has changed
+
+import React, {useEffect, useRef, useState} from 'react';
 import {
   StyleSheet,
   View,
@@ -22,10 +24,11 @@ import FaceSDK, {
 } from '@regulaforensics/react-native-face-api';
 import LinearGradient from 'react-native-linear-gradient';
 import axios from 'axios';
+import ImagePicker from 'react-native-image-crop-picker';
 import RNFS from 'react-native-fs';
 
 LogBox.ignoreLogs(['new NativeEventEmitter']);
-// const eventManager = new NativeEventEmitter(RNFaceApi);
+const eventManager = new NativeEventEmitter(RNFaceApi);
 
 var image1 = new MatchFacesImage();
 
@@ -37,11 +40,46 @@ const Register = () => {
   const [tempUri, setTempUri] = useState(null);
   const [base64Image, setBase64Image] = useState('');
   const [testImage, setTestImage] = useState('');
+  const [cropImage, setCropImage] = useState(
+    require('../Images/ChrissEvans.jpg'),
+  );
+  const [imageUri, setImageUri] = useState({uri: ''});
+
+  // const [image, setImage] = useState('');
+  const [image, setImage] = useState(require('../Images/ChrissEvans.jpg'));
 
   useEffect(() => {
     const videoEncoderCompletionEvent = json => {
       const response = JSON.parse(json);
+      const transactionId = response['transactionId'];
+      const success = response['success'];
+      // console.log('video_encoder_completion:');
+      // console.log('    success: ' + success);
+      // console.log('    transactionId: ' + transactionId);
     };
+    // if (base64Image) {
+    //   convertBase64ToURI(base64Image)
+    //     .then(uri => {
+    //       setTempUri(uri);
+    //     })
+    //     .catch(error => {
+    //       console.log('Error converting base64 to URI:', error);
+    //     });
+    // }
+    const base64String = 'Your base64 string here';
+
+    // convertBase64ToURI(base64Image)
+    //   .then(uri => {
+    //     setTempUri(uri);
+    //   })
+    //   .catch(error => {
+    //     console.log('Error converting base64 to URI:', error);
+    //   });
+
+    eventManager.addListener(
+      'videoEncoderCompletionEvent',
+      videoEncoderCompletionEvent,
+    );
 
     FaceSDK.init(
       json => {
@@ -76,9 +114,15 @@ const Register = () => {
                 const response = FaceCaptureResponse.fromJson(
                   JSON.parse(result),
                 );
-                setImage(response.image.bitmap);
-
+                setImage(
+                  // FaceCaptureResponse.fromJson(JSON.parse(result))?.image
+                  //   ?.imageData,
+                  // Enum.ImageType.LIVE,
+                  response.image.bitmap,
+                );
+                // setImage(response.image.bitmap);
                 saveToGallery(response.image.bitmap);
+                // setTestImage(response.image.bitmap);
               },
               e => {
                 console.log(e);
@@ -90,11 +134,13 @@ const Register = () => {
     );
   };
 
-  const setImage = base64 => {
+  const setImageFAceApi = base64 => {
     if (base64 == null) return;
     try {
       image1.bitmap = base64;
       setImg1({uri: 'data:image/png;base64,' + base64});
+      setTestImage({uri: 'data:image/png;base64,' + image1.bitmap});
+      setBase64Image({uri: 'data:image/png;base64,' + base64});
       obj[0].image_url = 'data:image/jpeg;base64,' + image1.bitmap;
       obj[0].code = rollNo;
     } catch (error) {
@@ -135,8 +181,23 @@ const Register = () => {
       console.log('Error saving image to gallery:', error);
     }
   };
+  // const convertBase64ToURI = async base64String => {
+  //   try {
+  //     const tempFilePath = `${RNFS.TemporaryDirectoryPath}/tempImage.jpeg`;
+  //     await RNFS.writeFile(tempFilePath, base64String, 'base64');
+  //     setTempUri(tempFilePath);
+  //     console.log('Path is', tempFilePath);
+  //     return `file://${tempFilePath}`;
+  //   } catch (error) {
+  //     console.log('Error converting base64 to URI:', error);
+  //     throw error;
+  //   }
+  // };
 
   const handelSubmit = () => {
+    cropLast();
+    console.log('wait');
+    return;
     const headers = {
       'Content-Type': 'application/json',
       'Content-Length': ['81992924'],
@@ -148,22 +209,109 @@ const Register = () => {
         headers: headers,
       })
       .then(function (response) {
+        // handle success
         console.log('sent sucessfully', response);
+        // alert(JSON.stringify(response.data));
       })
       .catch(function (error) {
+        // handle error
         console.log(error);
+        // alert(error.message);
       });
   };
+
+  // const exampleImageUri = Image.resolveAssetSource(img1).uri;
+  // const CropImage = () => {
+  //   if (!tempUri) {
+  //     return Alert.alert(
+  //       'No image',
+  //       'Before open cropping only, please select image',
+  //     );
+  //   } else {
+  //   }
+
+  //   ImagePicker.openCropper({
+  //     uri: exampleImageUri,
+  //     width: 200,
+  //     height: 200,
+  //   })
+  //     .then(image => {
+  //       console.log('received cropped image', image);
+  //       // setImage({
+  //       //   uri: image.path,
+  //       //   width: image.width,
+  //       //   height: image.height,
+  //       //   mime: image.mime,
+  //       // });
+  //       // setImages(null);
+  //     })
+  //     .catch(e => {
+  //       console.log(e);
+  //       Alert.alert(e.message ? e.message : e);
+  //     });
+  // };
+
+  const pickSingleWithCamera = () => {
+    ImagePicker.openCamera({
+      // cropping: true,
+      width: 500,
+      height: 500,
+      includeExif: true,
+      includeBase64: true,
+
+      //
+    }).then(image => {
+      console.log('received image', image.data);
+      // cropImage.uri = image.path;
+      setCropImage({uri: 'data:image/jpg;base64,' + image.data});
+      imageUri.uri = image.path;
+      // setCropImage({
+      //   uri: image.path,
+      //   width: image.width,
+      //   height: image.height,
+      //   mime: image.mime,
+      // });
+      // setImages(null);
+    });
+    // .catch(e => alert(e));
+  };
+  const cropLast = () => {
+    if (!image) {
+      return Alert.alert(
+        'No image',
+        'Before open cropping only, please select image',
+      );
+    }
+
+    ImagePicker.openCropper({
+      path: imageUri.uri,
+      width: 200,
+      height: 200,
+      cropperCircleOverlay: true,
+    })
+      .then(image => {
+        console.log('received cropped image', image);
+        CroppedImage;
+        setImage({
+          uri: image.path,
+          width: image.width,
+          height: image.height,
+          mime: image.mime,
+        });
+        setImages(null);
+      })
+      .catch(e => {
+        console.log(e);
+        Alert.alert(e.message ? e.message : e);
+      });
+  };
+  // console.log(JSON.stringify(img1));
 
   return (
     <LinearGradient colors={['#e9b7ce', '#32c4c0', '#d3f3f1']}>
       <View style={styles.container}>
         <View>
-          <View>
-            {tempUri && (
-              <Image source={{uri: `file://${tempUri}`}} style={styles.image} />
-            )}
-          </View>
+          <View></View>
         </View>
 
         <View
@@ -232,11 +380,12 @@ const Register = () => {
             }}>
             <View
               style={{
+                flexDirection: 'row',
                 justifyContent: 'center',
                 alignItems: 'center',
                 width: '80%',
               }}>
-              <Text
+              {/* <Text
                 style={{
                   fontSize: 16,
                   marginBottom: 30,
@@ -244,7 +393,25 @@ const Register = () => {
                   color: 'black',
                 }}>
                 Click below to capture
-              </Text>
+              </Text> */}
+              <TouchableOpacity
+                onPress={() => {
+                  if (rollNo !== '') {
+                    pickSingleWithCamera(true);
+                  } else {
+                    Alert.alert('Warning', 'Please enter roll no');
+                  }
+                }}>
+                <Image
+                  style={{
+                    height: 200,
+                    width: 200,
+                    resizeMode: 'center',
+                  }}
+                  source={cropImage}
+                  resizeMode="center"
+                />
+              </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => {
                   if (rollNo !== '') {
@@ -255,17 +422,18 @@ const Register = () => {
                 }}>
                 <Image
                   style={{
-                    height: 250,
-                    width: 250,
+                    height: 200,
+                    width: 200,
                     resizeMode: 'center',
                   }}
-                  source={img1}
-                  resizeMode="contain"
+                  source={cropImage}
+                  resizeMode="center"
                 />
               </TouchableOpacity>
             </View>
           </View>
         </View>
+        {/* {img1 && <Image source={img1} style={styles.image} />} */}
         <View style={{}}>
           <View
             style={{
