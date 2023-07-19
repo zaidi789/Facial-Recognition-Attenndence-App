ade959c30862055d7c2e9697e60e003548b162ab
Zaid Rafiq <zaidrafiq11@gmail.com>
2023-07-19 17:43:31 +0500

    image compression screen cleaned

--- src/Screens/ImageCompression.js
+++ src/Screens/ImageCompression.js
@@ -39,6 +39,7 @@ var image1 = new MatchFacesImage();
 const ImageCompression = () => {
   const [img1, setImg1] = useState('');
   const [preview, setPreview] = useState(false);
+  const [size, setSize] = useState('');
 
   useEffect(() => {
     const videoEncoderCompletionEvent = json => {
@@ -79,10 +80,6 @@ const ImageCompression = () => {
               setPreview(true);
               setImage(response.assets[0].base64);
               image1.bitmap = response.assets[0].base64;
-              // setImg1({
-              //   uri: 'data:image/jpeg;base64,' + response.assets[0].base64,
-              // });
-              // console.log(img1);
             }),
         },
         {
@@ -122,6 +119,7 @@ const ImageCompression = () => {
       );
       setImg1(compressedImage.uri);
       console.log('--Compressed Image--', compressedImage.size);
+      setSize(compressedImage.size);
       const compressedBase64 = await convertToBase64(compressedImage.uri);
       console.log('--Compressed Image--', compressedBase64);
       // Use the compressed image base64 string as needed
@@ -139,150 +137,6 @@ const ImageCompression = () => {
       throw new Error('Failed to convert to base64');
     }
   };
-
-  ////////////////////////////////////////////////////////
-  ////////////ERRRRRRROOOOOOOOOOOORRRRRRRR////////////
-  //   const setImage = async base64 => {
-  //     try {
-  //       const compressedImage = await ImageResizer.createResizedImage(
-  //         `data:image/png;base64,${base64}`,
-  //         800, // desired width
-  //         800, // desired height
-  //         'JPEG',
-  //         80, // quality (0-100)
-  //       );
-
-  //       const compressedBase64 = await convertToBase64(compressedImage.uri);
-  //       console.log('--Compressed Image--', compressedBase64);
-  //       // Use the compressed image base64 string as needed
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   const convertToBase64 = uri => {
-  //     return new Promise((resolve, reject) => {
-  //       const xhr = new XMLHttpRequest();
-  //       xhr.onload = function () {
-  //         const base64 = xhr.responseText;
-  //         resolve(base64);
-  //       };
-  //       xhr.onerror = function () {
-  //         reject(new Error('Failed to convert to base64'));
-  //       };
-  //       xhr.open('GET', uri);
-  //       xhr.responseType = 'text';
-  //       xhr.send();
-  //     });
-  //   };
-  /////////////////////////////////////////////////
-  //   const setImage = async image => {
-  //     try {
-  //       const compressedImage = await ImageCropPicker.cleanSingle(image, {
-  //         compressImageMaxWidth: 800, // desired width
-  //         compressImageMaxHeight: 800, // desired height
-  //         compressImageQuality: 0.8, // Adjust the quality as desired (0-1)
-  //       });
-
-  //       console.log('--Compressed Image--', compressedImage);
-  //       // Access compressedImage.data for the base64 representation of the compressed image
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   const setImage = async base64 => {
-  //     if (base64 == null) return;
-  //     try {
-  //       const compressedImage = await ImageResizer.createResizedImage(
-  //         `data:image/png;base64,${base64}`,
-  //         800, // desired width
-  //         800, // desired height
-  //         'JPEG',
-  //         80, // quality (0-100)
-  //       );
-
-  //       const compressedBase64 = await ImageResizer.createResizedImage(
-  //         compressedImage.uri,
-  //         compressedImage.width,
-  //         compressedImage.height,
-  //         'JPEG',
-  //         80, // quality (0-100)
-  //         0, // rotation
-  //         undefined, // output path (optional)
-  //         'base64', // output format
-  //       );
-
-  //       console.log('--Compressed Image--', compressedBase64);
-  //       // Use the compressed image base64 string as needed (e.g., setImg1(compressedBase64))
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  /////////////////////////////////////////////////////////////////////////////
-  //   const setImage = async base64 => {
-  //     if (base64 == null) return;
-  //     try {
-  //       const compressedImage = await ImageResizer.createResizedImage(
-  //         `data:image/png;base64,${base64}`,
-  //         512, // desired width
-  //         512, // desired height
-  //         'JPEG',
-  //         80,
-  //          // quality (0-100)
-  //       );
-  //       setImg1(compressedImage.uri);
-
-  //       console.log('--Compressed Image--', compressedImage);
-  //       // Use the compressed image URI as needed (e.g., setImg1(compressedImage.uri))
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  // try {
-  //   image1.bitmap = base64;
-  //   setImg1({uri: 'data:image/png;base64,' + base64});
-  //   const result = await Image.compress(img1, {
-  //     compressionMethod: 'auto',
-  //   });
-  //   console.log('--Compressed Image--', result);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  //   };
-
-  //   const saveToGallery = async base64 => {
-  //     if (base64) {
-  //       try {
-  //         const filePath = `${
-  //           RNFS.ExternalDirectoryPath
-  //         }/FaceAttendanceApp_${Date.now()}.png`;
-
-  //         await RNFS.writeFile(filePath, base64, 'base64');
-  //         saveImageToGallery(filePath);
-
-  //         Alert.alert('Success', 'Image saved to gallery.');
-  //       } catch (error) {
-  //         console.log('Error saving image to gallery:', error);
-  //         Alert.alert('Error', 'Failed to save image to gallery.');
-  //       }
-  //     }
-  //   };
-
-  //   const saveImageToGallery = async filePath => {
-  //     try {
-  //       if (Platform.OS === 'android') {
-  //         await RNFS.scanFile(filePath);
-  //         console.log('sucessfully saved image');
-  //       } else {
-  //         await RNFS.copyAssetsFileIOS(
-  //           filePath,
-  //           RNFS.LibraryDirectoryPath + filePath,
-  //         );
-  //       }
-  //     } catch (error) {
-  //       console.log('Error saving image to gallery:', error);
-  //     }
-  //   };
 
   return (
     <LinearGradient colors={['#ddb4f6', '#8dd0fc']}>
@@ -316,66 +170,54 @@ const ImageCompression = () => {
             }}>
             Complete below sections
           </Text>
+          <Text
+            style={{
+              fontSize: 20,
+              marginTop: 0,
+              marginBottom: 20,
+              fontWeight: 'bold',
+              color: 'black',
+            }}>
+            Size:{size / 1024} kb
+          </Text>
         </View>
         <TouchableOpacity
           onPress={() => {
             pickImage(true);
           }}>
           <View
-            style={{
-              // backgroundColor: 'green',
-              // flex: 1,
-              height: '50%',
-              width: '100%',
-              marginBottom: 10,
-              borderWidth: 2,
-              borderStyle: 'dashed',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            {preview === false ? (
-              <View
+            style={
+              {
+                // backgroundColor: 'green',
+                // flex: 1,
+                //   height: '50%',
+                //   width: '100%',
+                //   marginBottom: 10,
+                //   borderWidth: 2,
+                //   borderStyle: 'dashed',
+                //   justifyContent: 'center',
+                //   alignItems: 'center',
+              }
+            }>
+            <Text>Click below box to capture of select</Text>
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderWidth: 1,
+              }}>
+              <Image
                 style={{
-                  // flex: 1,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: 0,
-                  marginBottom: 40,
-                  //   height: 250,
-                }}>
-                <TouchableOpacity
-                  onPress={() => {
-                    pickImage(true);
-                  }}>
-                  <Ionicons
-                    name="cloud-upload-outline"
-                    size={75}
-                    color="blue"
-                  />
-                </TouchableOpacity>
-                <Text
-                  style={{
-                    color: 'black',
-                    fontSize: 20,
-                    fontWeight: 'bold',
-                  }}>
-                  Click to Select / Capture
-                </Text>
-              </View>
-            ) : (
-              <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                <Image
-                  style={{
-                    height: 200,
-                    width: 200,
-                    resizeMode: 'center',
-                    // marginTop: 2,
-                  }}
-                  source={{uri: img1}}
-                  resizeMode="contain"
-                />
-              </View>
-            )}
+                  height: 300,
+                  width: 300,
+                  resizeMode: 'center',
+                  //   borderWidth: 1,
+                  // marginTop: 2,
+                }}
+                source={{uri: img1}}
+                resizeMode="contain"
+              />
+            </View>
           </View>
         </TouchableOpacity>
         <View
@@ -412,11 +254,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     padding: 10,
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#ffafcc',
-    // marginBottom: 12,
   },
   welcome: {
     fontSize: 20,
